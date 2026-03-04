16ea2b85ffa84d0ee3c2f7ed6cae4e8d6c245380
onedreamcwb <gustavo.luanaraujo2018@gmail.com>
2026-03-04 06:22:42 -0300

    add configuration constants for financial categories and reserve months

---
+++ js/core/constants.js
@@ -0,0 +1,11 @@
+const CONFIG = {
+    CATEGORIAS_SAIDA: [
+        "Mercado geral", "Restaurante e bares", "Moradia",
+        "Saúde", "Dependentes", "Serviços", "Impostos",
+        "Entretenimento", "Transporte", "Outros"
+    ],
+    CATEGORIAS_ENTRADA: [
+        "Trabalho", "Extra", "Investimento", "Aposentadoria"
+    ],
+    META_RESERVA_MESES: 6 // Sua regra de negócio
+};
